6a5de083038f9158edadab80e6a965fd64469150
vighate <103872772+vighate@users.noreply.github.com>
2026-07-13 15:05:43 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,89 @@
+from collections import Counter
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+
+        if len(s) < len(t):
+            return ""
+
+        need = Counter(t)
+        need_len = len(need)
+        window = {}
+        have = 0
+        res_len = float('inf')
+        res = [-1,-1]
+        left = 0
+
+        for right in range(len(s)):
+
+            c = s[right]
+            window[c] = window.get(c,0) + 1
+
+            if c in need and window[c] == need[c]:
+                have += 1
+            
+            while have == need_len:
+                if right-left+1 < res_len:
+                    res_len = right-left+1
+                    res = [left, right]
+
+                left_char = s[left]
+                window[left_char] -= 1
+
+                if left_char in need and window[left_char] < need[left_char]:
+                    have -= 1
+                left+=1
+
+
+        l,r = res
+        return s[l:r+1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        # if len(s) < len(t):
+        #     return ""
+
+        # need = Counter(t)
+        # need_len = len(need)
+
+        # res = [-1,-1]
+        # window = {}
+        # res_len = float('inf')
+        # have = 0
+        # left = 0
+
+        # for right in range(len(s)):
+        #     c = s[right]
+        #     window[c] = window.get(c,0) +1
+
+        #     if c in need and window[c] == need[c]:
+        #         have += 1
+            
+        #     while have == need_len:
+
+        #         if right-left+1 < res_len:
+        #             res_len = right-left+1
+        #             res = [left, right]
+
+        #         left_char = s[left]
+        #         window[left_char] -= 1
+
+        #         if left_char in need and window[left_char] < need[left_char]:
+        #             have -= 1
+        #         left += 1
+
+        # l,r = res
+        # return s[l:r+1]
