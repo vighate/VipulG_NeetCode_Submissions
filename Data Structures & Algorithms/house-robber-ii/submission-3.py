76c181b4a06b6e8b941baeead345c1a0638a2363
vighate <103872772+vighate@users.noreply.github.com>
2026-07-15 21:34:35 +0530

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.py
@@ -0,0 +1,61 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        if len(nums) == 0:
+            return 0
+
+        if len(nums) == 1:
+            return nums[0]
+
+        def house_rob(nums):
+            memo = {}
+
+            def dfs(i):
+
+                if i>=len(nums):
+                    return 0
+
+                if i in memo:
+                    return memo[i]
+
+                take = nums[i] + dfs(i+2)
+                skip = dfs(i+1)
+
+                memo[i] = max(take,skip)
+                return memo[i]
+            
+            return dfs(0)
+
+        return max(house_rob(nums[1:]), house_rob(nums[:-1]))
+
+        # if len(nums) == 0:
+        #     return 0
+
+        # if len(nums) == 1:
+        #     return nums[0]
+
+        # def house_rob(nums):
+
+        #     memo = {}
+
+        #     def dfs(i):
+
+        #         if i>=len(nums):
+        #             return 0
+
+        #         if i in memo:
+        #             return memo[i]
+
+        #         take = nums[i]+dfs(i+2)
+        #         skip = dfs(i+1)
+
+        #         memo[i] = max(take, skip)
+        #         return memo[i]
+
+        #     return dfs(0)
+
+
+        # return max(house_rob(nums[:-1]), house_rob(nums[1:]))
+
+
+        
