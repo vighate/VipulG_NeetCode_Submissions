65b5629fefb84e2cbdea6ec15f553a4e71e42696
vighate <103872772+vighate@users.noreply.github.com>
2026-07-15 21:37:54 +0530

    Add: longest-palindromic-substring - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.py
@@ -0,0 +1,68 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+
+        if len(s) == 0:
+            return ""
+
+        if len(s) == 1:
+            return s
+
+        memo = {}
+        def palindromic(left, right):
+
+            if left>=right:
+                return True
+
+            if (left, right) in memo:
+                return memo[(left, right)]
+
+            if s[left] != s[right]:
+                memo[(left, right)] = False
+
+            else:
+                memo[(left, right)] = palindromic(left+1, right-1)
+
+
+            return memo[(left, right)]
+        
+        max_len = 0
+        start = 0
+
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+
+                if palindromic(i,j) == True:
+
+                    if j-i+1 > max_len:
+                        max_len = j-i+1
+                        start = i
+        
+        return s[start: start+max_len]
+        
+        # memo = {}
+
+        # def dfs(left, right):
+
+        #     if left >= right:
+        #         return True
+
+        #     if (left, right) in memo:
+        #         return memo[(left, right)]
+
+        #     if s[left] != s[right]:
+        #         memo[(left, right)] = False
+        #     else:
+        #         memo[(left, right)] = dfs(left+1, right-1)
+
+        #     return memo[(left, right)]
+
+        # max_len = 0
+        # start = 0
+        # for i in range(len(s)):
+        #     for j in range(i, len(s)):
+        #         if dfs(i,j):
+        #             if j-i+1 > max_len:
+        #                 max_len = j-i+1
+        #                 start = i
+
+        # return s[start:start+max_len]
