c52516c13416417a6f6e73ca4bd8b731ea06df5c
vighate <103872772+vighate@users.noreply.github.com>
2026-07-15 11:33:58 +0530

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,97 @@
+from collections import Counter
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        if len(s1) > len(s2):
+            return False
+
+        s1_count = Counter(s1)
+        print(s1_count)
+
+        n = len(s1)
+        m = len(s2)
+
+        window = Counter(s2[:n])
+
+        if window == s1_count:
+            return True
+
+        for i in range(n,m):
+            c = s2[i]
+
+            window[c] += 1
+            window[s2[i-n]] -= 1
+
+            if window[s2[i-n]] == 0:
+                del window[s2[i-n]]
+            
+            if window == s1_count:
+                return True
+
+        return False
+
+
+
+
+
+
+
+
+
+
+
+
+        # if len(s1) > len(s2):
+        #     return False
+
+        # n = len(s1)
+        # m = len(s2)
+
+        # s1_counter = Counter(s1)
+
+        # window_counter = Counter(s2[:n])
+
+        # if window_counter == s1_counter:
+        #     return True
+        
+        # for i in range(n,m):
+
+        #     window_counter[s2[i]] += 1
+        #     window_counter[s2[i-n]] -= 1
+
+        #     if window_counter[s2[i-n]] == 0:
+        #         del window_counter[s2[i-n]]
+
+        #     if window_counter == s1_counter:
+        #         return True
+
+
+        # return False
+
+        # if len(s1) > len(s2):
+        #     return False
+
+        # s1_counter = Counter(s1)
+        # print(s1_counter)
+
+        # n = len(s1)
+        # m = len(s2)
+
+        # window_counter = Counter(s2[:n])
+        # print(window_counter)
+
+        # if window_counter == s1_counter:
+        #     return True
+
+        # for i in range(n,m):
+        #     window_counter[s2[i]] += 1
+        #     window_counter[s2[i-n]] -= 1
+
+        #     if window_counter[s2[i-n]] == 0:
+        #         del window_counter[s2[i-n]]
+
+        #     if window_counter == s1_counter:
+        #         return True
+
+        # return False
