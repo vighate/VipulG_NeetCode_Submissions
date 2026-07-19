8474474867e51235a3a6d4e258f3fbef723fe215
vighate <103872772+vighate@users.noreply.github.com>
2026-07-19 18:21:40 +0530

    Add: jump-game - submission-8

---
+++ Data Structures & Algorithms/jump-game/submission-8.py
@@ -0,0 +1,61 @@
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+
+        flag = True
+
+        maxReach = 0
+
+        for i in range(len(nums)):
+            if i > maxReach:
+                flag = False
+            maxReach = max(maxReach, i+nums[i])
+            
+        return flag
+
+
+
+        maxreach = 0
+        for i in range(len(nums)):
+
+            if i > maxreach:
+                return False
+
+            maxreach = max(maxreach, nums[i]+i)
+
+        return True
+
+
+
+
+
+        # maxReach = 0
+
+        # for i in range(len(nums)):
+
+        #     if i > maxReach:
+        #         return False
+
+        #     maxReach = max(maxReach, nums[i]+i)
+
+        # return True
+
+        # # memo = {}
+
+        # # def dfs(i):
+
+        # #     if i>=len(nums)-1:
+        # #         return True
+
+        # #     if i in memo:
+        #         return memo[i]
+
+        #     for jump in range(1, nums[i]+1):
+        #         if dfs(i+jump):
+        #             memo[i] = True
+        #             return True
+
+        #     memo[i] = False
+        #     return False
+
+        # return dfs(0)
