f55fe3e0dfbeb6575c353ecf655484fc09ce48cf
vighate <103872772+vighate@users.noreply.github.com>
2026-07-29 21:50:09 +0530

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.py
@@ -0,0 +1,51 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        adj = {i: [] for i in range(numCourses)}
+
+        for course, pre in prerequisites:
+            adj[course].append(pre)
+
+        path = set()
+        def dfs(course):
+            if course in path:
+                return False
+            path.add(course)
+            for pre in adj[course]:
+                if dfs(pre)==False:
+                    return False
+            path.remove(course)
+            return True
+
+
+        for i in range(numCourses):
+            if dfs(i) == False:
+                return False
+        return True
+
+
+
+        adj = {i: [] for i in range(numCourses)}
+
+        for course, pre in prerequisites:
+            adj[course].append(pre)
+        
+        path = set()
+
+        def dfs(course):
+
+            if course in path:
+                return False
+            path.add(course)
+            for pre in adj[course]:
+                if dfs(pre)==False:
+                    return False
+            
+            path.remove(course)
+            return True
+        
+        for i in range(numCourses):
+            if dfs(i) ==False:
+                return False
+        
+        return True
