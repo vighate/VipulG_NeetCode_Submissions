6eb16c4118c3781e6430c7bd96573b05f16e2219
vighate <103872772+vighate@users.noreply.github.com>
2026-07-30 15:09:37 +0530

    Add: word-ladder - submission-7

---
+++ Data Structures & Algorithms/word-ladder/submission-7.py
@@ -0,0 +1,71 @@
+from collections import deque
+from collections import defaultdict
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        
+        if endWord not in wordList:
+            return 0
+        
+        adj = defaultdict(list)
+        for word in wordList:
+            for i in range(len(word)):
+                pattern = word[:i]+"*"+word[i+1:]
+                adj[pattern].append(word)
+
+        q = deque([(beginWord, 1)])
+        visited = {beginWord}
+
+        while q:
+            word, steps = q.popleft()
+
+            if endWord == word:
+                return steps
+
+            for i in range(len(word)):
+                pattern = word[:i]+"*"+word[i+1:]
+
+                for nei in adj[pattern]:
+                    if nei not in visited:
+                        visited.add(nei)
+                        q.append((nei, steps+1))
+
+        return 0
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        if endWord not in wordList:
+            return 0
+        
+        visited = {beginWord}
+        adj = defaultdict(list)
+        for word in wordList:
+            for i in range(len(word)):
+                pattern = word[:i]+"*"+word[i+1:]
+                adj[pattern].append(word)
+        
+        q = deque([(beginWord,1)])
+        while q:
+            word, level = q.popleft()
+            if word == endWord:
+                return level
+            for i in range(len(word)):
+                pattern = word[:i]+"*"+word[i+1:]
+                for nei in adj[pattern]:
+                    if nei not in visited:
+                        visited.add(nei)
+                        q.append((nei, level+1))
+        return 0
+                        
+
