9e80c0d9344e2612494f551eb13413d7af04884b
vighate <103872772+vighate@users.noreply.github.com>
2026-07-30 11:27:18 +0530

    Add: count-number-of-islands - submission-7

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-7.py
@@ -0,0 +1,27 @@
+from collections import deque
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        islands = 0
+        rows = len(grid)
+        cols = len(grid[0])
+
+        if not grid:
+            return 0
+        
+        def dfs(r,c):
+            if (r<0 or r>=rows or c<0 or c>=cols or grid[r][c] == "0"):
+                return
+            grid[r][c] = "0"
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c-1)
+            dfs(r,c+1)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    islands +=1
+                    dfs(r,c)
+
+        return islands
