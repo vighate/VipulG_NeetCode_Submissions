5a38df066fbe695bba89342215c4cbad656570b8
vighate <103872772+vighate@users.noreply.github.com>
2026-08-13 11:35:37 +0530

    Add: subsets-ii - submission-3

---
+++ Data Structures & Algorithms/subsets-ii/submission-3.py
@@ -0,0 +1,47 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        
+        #nums = sorted(nums, key = lambda x:x)
+        nums = sorted(nums)
+        res = []
+        path = []
+
+        def dfs(start):
+            res.append(path[:])
+            
+            # if start == len(nums):
+            #     res.append(path[:])
+            #     return
+            
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i-1]:
+                    continue
+                path.append(nums[i])
+                dfs(i+1)
+                path.pop()
+
+        dfs(0)
+        return res    
+
+
+
+
+        res = []
+        path = []
+
+        nums = sorted(nums)
+
+        
+        def dfs(start):
+            res.append(path[:])
+            
+            for i in range(start, len(nums)):
+                if i>start and nums[i] == nums[i-1]:
+                    continue
+
+                path.append(nums[i])
+                dfs(i+1)
+                path.pop()
+
+        dfs(0)
+        return res
