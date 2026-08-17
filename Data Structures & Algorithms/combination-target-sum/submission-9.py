05da44d8b298af502e2c93492488bf465a2c7221
vighate <103872772+vighate@users.noreply.github.com>
2026-08-17 12:54:08 +0530

    Add: combination-target-sum - submission-9

---
+++ Data Structures & Algorithms/combination-target-sum/submission-9.py
@@ -0,0 +1,87 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+
+
+        res = []
+        path = []
+
+        def dfs(i, remaining):
+            if remaining == 0:
+                res.append(path[:])
+                return
+
+            if remaining < 0 or i == len(nums):
+                return
+
+            path.append(nums[i])
+            dfs(i, remaining-nums[i])
+            path.pop()
+            dfs(i+1, remaining)
+
+        dfs(0, target)
+        return res
+
+
+
+
+        res = []
+        path = []
+
+        def dfs(i, remaining):
+            if remaining == 0:
+                res.append(path[:])
+                return
+            
+            if remaining < 0 or i == len(nums):
+                return
+            
+            path.append(nums[i])
+            dfs(i, remaining-nums[i])
+            path.pop()
+            dfs(i+1, remaining)
+
+        dfs(0,target)
+        return res
+
+        res = []
+        path = []
+
+        def dfs(i, remaining):
+
+            if remaining == 0:
+                res.append(path[:])
+                return
+            
+            if remaining < 0 or i == len(nums):
+                return 
+            
+            path.append(nums[i])
+            dfs(i, remaining-nums[i])
+            path.pop()
+            dfs(i+1, remaining)
+
+        dfs(0,target)
+        return res
+
+
+        
+        res = []
+        path = []
+
+        def dfs(i, remaining):
+
+            if remaining == 0:
+                res.append(path[:])
+                return
+            
+            if remaining < 0 or i==len(nums):
+                return
+            
+            path.append(nums[i])
+            dfs(i, remaining-nums[i])
+            path.pop()
+            dfs(i+1, remaining)
+
+        
+        dfs(0, target)
+        return res
