35a909305c84cbc96739ad268f3e4600f112da8f
vighate <103872772+vighate@users.noreply.github.com>
2026-08-17 13:09:51 +0530

    Add: max-area-of-island - submission-6

---
+++ Data Structures & Algorithms/max-area-of-island/submission-6.py
@@ -0,0 +1,69 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+
+
+        rows = len(grid)
+        cols = len(grid[0])
+
+        max_area = 0
+        def dfs(r,c):
+            if (r<0 or r>=rows or c<0 or c>=cols or grid[r][c] == 0):
+                return 0
+            grid[r][c] = 0
+            ans = 1+ dfs(r+1,c) + dfs(r-1,c) + dfs(r,c-1) +dfs(r,c+1)
+            return ans
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    max_area = max(max_area, dfs(r,c))
+
+        return max_area
+
+
+        max_area = 0
+        rows = len(grid)
+        cols = len(grid[0])
+
+        if not grid:
+            return 0
+        def dfs(r,c):
+            if (r<0 or r>=rows or c<0 or c>=cols or grid[r][c] == 0):
+                return 0
+            grid[r][c] = 0
+            ans = 1+dfs(r+1,c)+dfs(r-1,c)+dfs(r,c-1)+dfs(r,c+1)
+            return ans
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    max_area = max(max_area, dfs(r,c))
+        return max_area
+
+
+
+        if not grid:
+            return 0
+        rows = len(grid)
+        cols = len(grid[0])
+
+        def dfs(r,c):
+            if (r<0 or r>=rows or c<0 or c>=cols or grid[r][c]==0):
+                return 0
+            
+            grid[r][c] = 0
+            area = 1
+            area += dfs(r+1,c)
+            area += dfs(r-1,c)
+            area += dfs(r,c+1)
+            area += dfs(r,c-1)
+
+            return area
+
+        max_area = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    max_area = max(max_area,dfs(i,j))
+        
+        
+        return max_area
