af25ae86658f2050e8b8ed3e55dc8aae19a96871
vighate <103872772+vighate@users.noreply.github.com>
2026-08-18 10:24:15 +0530

    Add: course-schedule - submission-10

---
+++ Data Structures & Algorithms/course-schedule/submission-10.py
@@ -0,0 +1,134 @@
+from collections import defaultdict
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+
+
+        adj = defaultdict(list)
+
+        for course, pre in prerequisites:
+            adj[course].append(pre)
+
+        vis = set()
+        path = set()
+
+        def dfs(course):
+
+            if course in path:
+                return False
+            if course in vis:
+                return True
+            path.add(course)
+            for pre in adj[course]:
+                if dfs(pre)==False:
+                    return False
+            path.remove(course)
+            vis.add(course)
+            return True
+        
+        for course in range(numCourses):
+            if dfs(course) == False:
+                return False
+
+        return True
+
+
+
+
+        
+        adj = {i: [] for i in range(numCourses)}
+        for course, pre in prerequisites:
+            adj[course].append(pre)
+        
+        path = set()
+        def dfs(node):
+            if node in path:
+                return False
+            path.add(node)
+
+            for nei in adj[node]:
+                if dfs(nei) == False:
+                    return False
+            path.remove(node)
+            return True
+
+        for i in range(numCourses):
+            if dfs(i) == False:
+                return False
+        return True
+
+
+
+
+
+        preMap = {i: [] for i in range(numCourses)}
+        path = set()
+        visited = set()
+
+        for course, pre in prerequisites:
+            preMap[course].append(pre)
+
+        def dfs(course):
+            # cycle detected
+            if course in path:
+                return False
+            if course in visited:
+                return True
+            path.add(course)
+
+            for pre in preMap[course]:
+                if dfs(pre)==False:
+                    return False
+            path.remove(course)
+            visited.add(course)
+            return True 
+
+        for course in range(numCourses):
+            if dfs(course) == False:
+                return False
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        preMap = {i: [] for i in range(numCourses)}
+
+        for course, pre in prerequisites:
+            preMap[course].append(pre)
+        
+        visited = set()
+
+        def dfs(course):
+
+            if course in visited:
+                return False
+
+            if preMap[course] ==[]:
+                return True
+
+            visited.add(course)
+
+            for pre in preMap[course]:
+                if dfs(pre) ==False:
+                    return False
+            
+            visited.remove(course)
+            preMap[course] =[]
+            return True
+        
+        for course in range(numCourses):
+            if dfs(course) == False:
+                return False
+        
+        return True
