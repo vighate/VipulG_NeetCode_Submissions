dde5f5d19564d9ba5a44bab9b99924fb54892744
vighate <103872772+vighate@users.noreply.github.com>
2026-08-18 10:25:10 +0530

    Add: course-schedule-ii - submission-9

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-9.py
@@ -0,0 +1,138 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+
+
+
+
+
+
+        
+        adj = defaultdict(list)
+
+        for course, pre in prerequisites:
+            adj[course].append(pre)
+
+        vis = set()
+        path = set()
+        order = []
+
+        def dfs(course):
+
+            if course in path:
+                return False
+            if course in vis:
+                return True
+            path.add(course)
+            for pre in adj[course]:
+                if dfs(pre)==False:
+                    return False
+            path.remove(course)
+            vis.add(course)
+            order.append(course)
+            return True
+        
+        for course in range(numCourses):
+            if dfs(course) == False:
+                return []
+
+        return order
+        
+        
+        adj = {i: [] for i in range(numCourses)}
+        for course, pre in prerequisites:
+            adj[course].append(pre)
+
+        path = set()
+        visited = set()
+        order = []
+
+        def dfs(course):
+            if course in path:
+                return False
+            if course in visited:
+                return True
+            path.add(course)
+            for nei in adj[course]:
+                if dfs(nei) == False:
+                    return False
+            path.remove(course)
+            visited.add(course)
+            order.append(course)
+
+        for i in range(numCourses):
+            if dfs(i) == False:
+                return []
+        return order
+
+
+
+
+
+
+
+
+
+        adj = {i: [] for i in range(numCourses)}
+        for course, pre in prerequisites:
+            adj[course].append(pre)
+        order = []
+        path = set()
+        visited = set()
+
+        def dfs(course):
+            if course in path:
+                return False
+            if course in visited:
+                return True
+            path.add(course)
+            for pre in adj[course]:
+                if dfs(pre) ==False:
+                    return False
+            path.remove(course)
+            visited.add(course)
+            order.append(course)
+            return True
+
+        for course in range(numCourses):
+            if dfs(course) == False:
+                return []
+        return order
+
+
+
+
+
+
+
+
+
+
+
+
+        preMap = {i: [] for i in range(numCourses)}
+        for course, pre in prerequisites:
+            preMap[course].append(pre)
+        visiting = set()
+        visited = set()
+        order = []
+        def dfs(course):
+            if course in visiting:
+                return False
+            if course in visited:
+                return True
+            visiting.add(course)
+            for pre in preMap[course]:
+                if dfs(pre) == False:
+                    return False
+
+            visiting.remove(course)
+            visited.add(course)
+            order.append(course)
+            return True
+
+        for course in range(numCourses):
+            if dfs(course) == False:
+                return []
+        
+        return order
+
